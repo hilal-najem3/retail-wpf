33e0b56538663a4e58be779281dea4ebd83ad905
hilal-najem <hilal123work@gmail.com>
2020-10-27 12:23:31 +0200

    Add The Login View

---
+++ Events/LogOnEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace retail_wpf.Events
+{
+    public class LogOnEvent
+    {
+    }
+}
--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,11 +1,35 @@
 ﻿using Caliburn.Micro;
+using retail_wpf.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Windows;
 
 namespace retail_wpf.ViewModels
 {
-    class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
+        public string ImagePath { get; set; }
+
+        public ShellViewModel()
+        {
+            StartApp();
+            
+        }
+
+        private void StartApp()
+        {
+            string projectPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))));
+            projectPath += "/Assets/Images/home.png";
+            ImagePath = projectPath;
+
+
+        }
+
+        public void Handle(LogOnEvent message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
